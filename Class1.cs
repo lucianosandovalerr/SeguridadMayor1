6d7e010e0b8af755116fd505fc807e9c5b4bd9fa
Luciano <Luciano@DESKTOP-2K3IJVU>
2017-05-12 17:51:21 -0300

    Clase de restricción

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegMayor
+{
+    class Class1
+    {
+        DateTime fechaTurno;
+        
+        DateTime[] feriados = new DateTime[]
+        {
+            //idealmente obtener días feriados por mes desde bbdd
+            new DateTime(2017, 03, 02),
+            new DateTime(2010, 03, 14),
+        };
+
+        
+    }
+
+}
