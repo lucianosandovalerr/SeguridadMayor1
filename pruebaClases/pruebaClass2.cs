51cbaf367d8d2fe42b14536f9cf2266f40f6ce31
victorgerard0 <geraaardo@gmail.com>
2017-05-16 20:31:20 -0400

    16.05

---
+++ pruebaClases/pruebaClass2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace pruebaClases
+{
+    [TestClass]
+    public class pruebaClass2
+    {
+        public void pruebaDias()
+        {
+
+
+            DateTime dateValue = new DateTime(2017, 6, 11);
+            Debug.WriteLine("OLAAAAAAAAAAAAAAAAAAA");
+
+
+
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+
+        }
+    }
+}
